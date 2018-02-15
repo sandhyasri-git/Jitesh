0cfc107cf147a23ab043d07c6b34b238a0b20079
sandhyasri-git <sandhyasri-git@users.noreply.github.com>
2018-02-15 10:31:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace AttributesDemo
+{
+    class Program
+    {
+        [Obsolete("outDated Method")]
+        public void show()
+        {
+            Console.WriteLine("Method called");
+        }
+        //static void Main(string[] args)
+        //{
+        //    Program p1 = new Program();
+        //    p1.show();
+        //}
+    }
+}
